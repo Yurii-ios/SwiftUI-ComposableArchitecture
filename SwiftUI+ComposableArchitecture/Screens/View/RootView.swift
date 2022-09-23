d676169b8e87e70a6bad30adde53a3847b3c560a
Yurii Sameliuk <sameliuk.itechart@gmail.com>
2022-09-23 09:44:17 +0200

    init project

--- SwiftUI+ComposableArchitecture/Screens/View/RootView.swift
+++ SwiftUI+ComposableArchitecture/Screens/View/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  SwiftUI+ComposableArchitecture
 //
 //  Created by Yurii.Semeliuk on 23/09/2022.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootView()
     }
 }
--- SwiftUI+ComposableArchitecture/SwiftUI_ComposableArchitectureApp.swift
+++ SwiftUI+ComposableArchitecture/SwiftUI_ComposableArchitectureApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_ComposableArchitectureApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
